5088a15b58771e21b74f3eb31d5faa3bf98503eb
Jho077 <jespinoza@cendrassos.net>
2024-04-15 01:41:29 +0200

    inicial

---
+++ resources/js/Components/CKEditor.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <ckeditor :editor="editor" v-model="editorData"></ckeditor>
+    </div>
+</template>
+
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    export default {
+        props: ['contentEditor', 'config'],
+        name: 'app',
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: this.contentEditor,
+            };
+        }
+    }
+</script>

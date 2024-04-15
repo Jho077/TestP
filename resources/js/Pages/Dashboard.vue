4a0d5223d89fd77191f4d2ec6b35466f2418149a
Jho077 <jespinoza@cendrassos.net>
2024-04-15 13:22:28 +0200

    hola

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -39,8 +39,9 @@ function handleImageError() {
   <div class="bg-gray-50 text-black/50 ">
 
 
-    
 
+
+    
     <div
       class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
       <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
